85259d5bf1695239555ba34279cf4847b6ce7979
Thiago Batista <cavalcantibatista@gmail.com>
2024-05-20 09:14:28 +0100

    test

--- api /src/main/java/com/fleet/api/controllers/UserControllers.java
+++ api /src/main/java/com/fleet/api/controllers/UserControllers.java
@@ -9,28 +9,27 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/")
 @CrossOrigin(origins = "*")
 public class UserControllers {
-    // TODO: Controller não pode chamar o repository, não é boa pratica, use service para bussines
+
     private final UserRepository userRepository;
 
     public UserControllers(UserRepository userRepository) {
         this.userRepository = userRepository;
     }
 
-    @GetMapping // TODO rever a forma correta de usar o Response Entity
+    @GetMapping
     public ResponseEntity getAllUsers(){
         return ResponseEntity.ok(this.userRepository.findAll());
     }
 
-    @PostMapping // TODO; the same up
+    @PostMapping
     public ResponseEntity save(@RequestBody UserEntity user) {
         try {
-            // Salvar o usuário no banco de dados usando o UserRepository
+
             UserEntity savedUser = userRepository.save(user);
 
-            // Retorna a resposta com status 201 Created e o objeto salvo
             return ResponseEntity.status(201).body(savedUser);
         } catch (Exception e) {
-            // Em caso de erro, retorna uma resposta com status 500 Internal Server Error
+
             return ResponseEntity.status(500).body("Erro ao salvar o usuário");
         }
     }
